df580441ec5a4ff76dc949fcd033ffa4fb0c9a87
marc095 <marc7990@gmail.com>
2017-04-14 18:29:11 +0300

    add dataaccess ninject module

--- WebCalendar/WebCalendar.DAL/Infrastructure/NinjectDataAccessModule.cs
+++ WebCalendar/WebCalendar.DAL/Infrastructure/NinjectDataAccessModule.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ninject.Activation;
+using Ninject.Infrastructure;
+using Ninject.Modules;
+using System;
+using WebCalendar.DAL.Concrete;
+using WebCalendar.DAL.Context;
+using WebCalendar.DAL.Helpers;
+using WebCalendar.DAL.Repositories;
+using WebCalendar.Domain.Aggregate.Calendar;
+using WebCalendar.Domain.Aggregate.Event;
+using WebCalendar.Domain.Aggregate.Notification;
+using WebCalendar.Domain.Aggregate.Occurrence;
 
 namespace WebCalendar.DAL.Infrastructure
 {
-  public  class NinjectDataAccessModule
+    public class NinjectDataAccessModule : NinjectModule
     {
+        private readonly Func<IContext, object> scope;
+
+
+        public NinjectDataAccessModule(Func<IContext, object> scope)
+        {
+            this.scope = scope;
+        }
+
+        public override void Load()
+        {
+            var configs = new IHaveBindingConfiguration[]
+                              {
+                                  Bind<IUnitOfWork>().To<UnitOfWork>(),
+                                  Bind<IEventRepository>().To<EventRepository>(),
+                                  Bind<ICalendarRepository>().To<CalendarRepository>(),
+                                  Bind<INotificationRepository>().To<NotificationRepository>(),
+                                  Bind<IOccurrenceRepository>().To<OccurenceRepository>()
+                              };
+
+            configs.InScope(scope);
+        }
     }
 }
