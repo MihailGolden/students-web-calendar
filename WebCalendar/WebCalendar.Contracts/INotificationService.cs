284017491c4ee28297061677fb70a2b1037efccf
marc095 <marc7990@gmail.com>
2017-04-24 16:31:00 +0300

    add services

---
+++ WebCalendar/WebCalendar.Contracts/INotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebCalendar.Domain.Aggregate.Notification;
+
+namespace WebCalendar.Contracts
+{
+    public interface INotificationService
+    {
+        void Create(Notification notify);
+        void AddNotifications(List<Notification> notifications);
+        Notification Get(int id);
+        List<Notification> GetNotifications { get; }
+        void Update(Notification notify);
+        void Delete(int id);
+    }
+}
---
+++ WebCalendar/WebCalendar.Contracts/IOccurrenceService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebCalendar.Domain.Aggregate.Occurrence;
+
+namespace WebCalendar.Contracts
+{
+    public interface IOccurrenceService
+    {
+        void Create(Occurrence occurrence);
+        void AddOccurrences(List<Occurrence> occurrences);
+        Occurrence Get(int id);
+        List<Occurrence> GetOccurrences { get; }
+        void Update(Occurrence occurrence);
+        void Delete(int id);
+    }
+}
